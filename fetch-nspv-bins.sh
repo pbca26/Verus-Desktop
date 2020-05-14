5e8cc5418ee59c28ad2cdaa4d080bbc51a22276a
pbca26 <pbca26@gmail.com>
2020-05-14 21:20:16 +0300

    fetch nspv bins fix typo, remove temp files after extract

--- fetch-nspv-bins.sh
+++ fetch-nspv-bins.sh
@@ -4,10 +4,13 @@ mkdir assets/bin/linux64
 mkdir assets/bin/osx
 cd assets/bin/win64
 wget https://github.com/SirSevenG/libnspv/releases/download/6/nspv-win-b79916e94095ff1dcdfd608cf8a2071f52d42893.zip
-unzip nspv-win-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
+unzip nspv-win-b79916e94095ff1dcdfd608cf8a2071f52d42893.zip
+rm nspv-win-b79916e94095ff1dcdfd608cf8a2071f52d42893.zip
 cd ../linux64
 wget https://github.com/SirSevenG/libnspv/releases/download/6/nspv-linux-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
 tar -xvzf nspv-linux-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
+rm nspv-linux-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
 cd ../osx
 wget https://github.com/SirSevenG/libnspv/releases/download/6/nspv-macos-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
 tar -xvzf nspv-macos-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
+rm nspv-macos-b79916e94095ff1dcdfd608cf8a2071f52d42893.tar.gz
