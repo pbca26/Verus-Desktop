34cbdeb5d9cfffceee5413c7bccb0a8081b05974
pbca26 <pbca26@gmail.com>
2020-06-05 14:20:36 +0300

    spv conn manager getProtocolVersion

---
+++ routes/api/electrum/connectionManager.js
@@ -0,0 +1,32 @@
+getProtocolVersion = (_ecl) => {
+  let protocolVersion;
+  
+  return new Promise((resolve, reject) => {
+    _ecl.serverVersion('VerusDesktop')
+    .then((serverData) => {
+      let serverVersion = 0;
+
+      if (serverData &&
+          typeof serverData === 'object' &&
+          serverData[0] &&
+          serverData[0].indexOf('ElectrumX') > -1 &&
+          Number(serverData[1])
+      ) {
+        serverVersion = Number(serverData[1]);
+
+        if (serverVersion) {            
+          protocolVersion = Number(serverData[1]);
+          _ecl.setProtocolVersion(protocolVersion.toString());
+        }
+      }
+
+      if (serverData.hasOwnProperty('code') &&
+          serverData.code === '-777') {
+        resolve(-777);
+      }
+
+      console.log(`ecl ${`${_ecl.host}:${_ecl.port}:${_ecl.protocol || 'tcp'}`} protocol version: ${protocolVersion}`);
+      resolve(protocolVersion);
+    });
+  });
+};
