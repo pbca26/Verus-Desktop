67b75c53cf40648faf6144c7f09834e4df19356e
Michael Toutonghi <toutonghi.michael@gmail.com>
2020-07-13 15:15:56 +0200

    Add warning indicating prices are based on last price

--- routes/api/native/send.js
+++ routes/api/native/send.js
@@ -177,10 +177,17 @@ module.exports = (api) => {
         // Extract reserve transfer outputs
         try {
           sendCurrencyTest = await api.native.testSendCurrency(chainTicker, txParams)
+
+          warnings.push({
+            field: "price",
+            message: 'All price estimations are based on the latest conversion price. The actual conversion result is calculated at the time of conversion and will most likely differ.'
+          });
         } catch(e) {
           if (e.message === 'Insufficient funds' && mint) {
             e.message = `Insufficient funds. To mint coins, ensure that the identity that created this currency (${fromAddress}) has at least a balance of 0.0002 ${chainTicker}.`
           } else {
+            api.log("Error while testing currency send!", "send")
+            api.log(e, "send")
             throw e
           }
         }
