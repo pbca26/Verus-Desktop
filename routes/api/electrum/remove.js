9a8efc599ffafbbde68738cd7e3cca77efc14bcd
pbca26 <pbca26@gmail.com>
2020-07-15 18:13:47 +0300

    lite mode coin removal fix

--- routes/api/electrum/remove.js
+++ routes/api/electrum/remove.js
@@ -10,7 +10,7 @@ module.exports = (api) => {
 
       delete api.electrum.coinData[_chain.toLowerCase()];
       
-      if (Object.keys(api.electrum.coinData).length - 1 === 0) {
+      if (Object.keys(api.electrum.coinData).length === 0) {
         api.electrumKeys = {};
       }
 
